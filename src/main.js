e56966c23b1426ed7358d10a8e2bac4d762c9c56
youlianlin <f152543275@gmail.com>
2023-09-10 21:01:41 +0800

    update file

--- src/main.js
+++ src/main.js
@@ -2,5 +2,9 @@ import "./assets/main.css"
 
 import { createApp } from "vue"
 import App from "./App.vue"
+import ItemComponent from '@/components/Item.vue';
 
-createApp(App).mount("#app")
+const app = createApp(App);
+app.component('ItemComponent',ItemComponent);
+app.mount("#app");
+//createApp(App).mount("#app")
